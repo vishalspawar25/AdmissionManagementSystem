55e5c2cc35cd1626aecf0b0b43e892ba6c79bc57
vishal pawar <vishalsapawar25@gmail.com>
2018-02-16 16:13:46 +0530

    Initial Commit

---
+++ AdmissionManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdmissionManagementSystem.Startup))]
+namespace AdmissionManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
